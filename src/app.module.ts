3ab16a9116c40765994ad5231235183217f7ebfd
sato-souma <59986133+sato-souma@users.noreply.github.com>
2022-06-07 15:00:43 +0900

    error

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
+import { PrismaService } from './prisma.service'
+import { UserService } from './user.service'
+import { PostService } from './post.service'
 
 //デコレータ クラスを改造する役割　クラスを受け取って設定する
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService, PostService],
 })
 export class AppModule {}
